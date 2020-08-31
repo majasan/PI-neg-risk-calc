f8f52b80bdbf7d21e02cca2389260adb112fd73b
majasan <45463797+majasan@users.noreply.github.com>
2020-08-31 18:28:58 -0400

    Update pinegriskcalcV2.py

--- pinegriskcalcV2.py
+++ pinegriskcalcV2.py
@@ -1,24 +1,41 @@
-
-import requests
-import urllib3
-import re
-
-conNumber = input('Enter Contract Number := ')
-urlMarket = 'https://www.predictit.org/api/marketdata/markets/'+conNumber
+import requests; import urllib3; import re; import sys
+urlMarket = 'https://www.predictit.org/api/marketdata/all/'
 response = requests.get(urlMarket)
-print(urlMarket)
-with open('/users/jamundur/Downloads/feedv3.txt','wb') as file:
+with open('/users/jamundur/Downloads/feedAll.txt','wb') as file:
     file.write(response.content)
-psum=0.0
-regexp = re.compile(r'      "bestSellYesCost".*?([0-9.-]+)') 
-with open('/users/jamundur/Downloads/feedv3.txt') as file:
+regexp1 = re.compile(r'      "id".*?([0-9.-]+)') 
+regexp2 = re.compile(r'          "bestSellYesCost".*?([0-9.-]+)') 
+regexp3 = re.compile(r'      "timeStamp".*?([0-9.-]+)')
+regexp4 = re.compile(r'      "name".*?([0-9.-]+)')
+print("""
+******************************************************************************
+\tThis program will print all contracts ( and their links) that are eligible
+for betting with no or negative risk.
+\tPlease read about it from Zvi Bodie at the link below. 
+\thttps://www.lesswrong.com/posts/qzRzQgxiZa3tPJJg8/free-money-at-predictit
+*******************************************************************************
+""")
+psum = 0.0;finalsum = 0.0
+with open('/users/jamundur/Downloads/feedAll.txt') as file:
     for line in file:
-        match = regexp.match(line)
-        if match:
-            psum=psum+float(match.group(1))  
-      
-print(" Total of all comes to :",round(psum,2))
-if psum >= 1.10 :
-    print( "This contract is ready for Negative Risk betting. Bet equal cash on all contracts within the market. Good luck !!!")
-else :
-    print(" This market and contracts are NOT ready for Negative Risk betting.")
+        match1 = regexp1.match(line); match2 = regexp2.match(line) ; match3 = regexp3.match(line)
+        psum=0.0
+        if match1:
+            cid = match1.group(1)  
+        if match2:
+            psum=psum+float(match2.group(1))
+            finalsum=finalsum+psum
+        if match3:
+            if round(finalsum,2) >=1.10:
+                print("\n\t\thttps://www.predictit.org/markets/detail/"+cid,"\t ",round(finalsum,2),"\n")
+            finalsum=0.0
+
+print("""
+*******************************************************************************
+\tMake sure you buy *equal* number of 'Buy No' contracts in *all* tranches for each 
+of the above contracts. 
+\tPlease read more before you play this betting strategy where you get paid 
+to bet !!!
+https://blog.rossry.net/predictit/
+*******************************************************************************
+""")
